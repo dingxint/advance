35a321533565606ddd8421f1959bb2ef5ee1f8dc
dingxintian <1440762598@qq.com>
2021-08-12 13:35:09 +0800

    2

--- src/main/java/com/desiree/advance/service/Multithreading.java
+++ src/main/java/com/desiree/advance/service/Multithreading.java
@@ -5,6 +5,8 @@ public class Multithreading {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println(133);
+        System.out.println(3);
+        System.out.println(4);
     }
 
 }
